7736299ad19224830b31ce29db5d35876f50361b
Sameer Kampa <kampasameer95@gmail.com>
2026-03-30 00:26:02 +0530

    Added Day 2 DSA problem

---
+++ .ipynb_checkpoints/day_1-checkpoint.cpp
@@ -0,0 +1,138 @@
+// fundamental syntax in c++
+
+        //    #include<iostream>
+        //    #include<math.h>
+        //    int main(){
+        //    std::cout << "Hello World!";
+        //    
+        //    return 0;
+        //    }
+
+// short to beat librery
+
+    // #include<bits/stdc++.h>
+    // using namespace std;
+    // int main(){
+    // cout << "Hello World!" << endl;
+    // 
+    // return 0;
+    // }
+
+// taking user input
+
+        // #include<bits/stdc++.h>
+        // using namespace std;
+        // 
+        // int main(){
+        // string s;
+        // cin >> s;
+        // 
+        // cout << s;
+            // return 0;
+        // }
+
+
+// taking user input
+
+        // #include<bits/stdc++.h>
+        // using namespace std;
+        // int main(){
+            // int s;
+            // cin >> s;
+        // 
+            // cout << s + "kampa" << endl;
+            // return 0;
+        // }
+
+
+// Arithmatic operation
+
+        // #include<bits/stdc++.h>
+        // using namespace std;
+        // int main(){
+        // int a;
+        // int b;
+        // cin >> a >> b;
+        // cout << a+b;
+        //     return 0;
+        // }
+
+
+
+//  if-else statement
+
+
+        // #include<bits/stdc++.h>
+        // using namespace std;
+        // int main(){
+            // int a;
+            // int b;
+            // cin >> a >> b;
+            // if ((a+b)>50){
+                // cout << "Selected";
+            // }
+            // else{
+                // cout << "unselected";
+            // }
+        // 
+            // return 0;
+        // }
+
+
+
+// using switch case
+
+
+        // #include<bits/stdc++.h>
+        // using namespace std;
+        // int main() {
+            // int month;
+            // cin >> month;
+            // switch(month){
+                // case 1:
+                // cout << "jan" << endl;break;
+            // 
+                // case 2:
+                // cout << "feb" << endl;break;
+            // 
+                // case 3:
+                // cout << "mar" << endl;break;
+            // 
+                // case 4:
+                // cout << "apr" << endl;break;
+            // 
+                // case 5:
+                // cout << "may" << endl;break;
+            // 
+            // 
+                // case 6:
+                // cout << "jun" << endl;break;
+            // 
+                // case 7:
+                // cout << "july" << endl;break;
+            // 
+            // 
+                // case 8:
+                // cout << "aug" << endl;break;
+            // 
+            // 
+                // case 9:
+                // cout << "sep" << endl;break;
+            // 
+            // 
+                // case 10:
+                // cout << "oct" << endl;break;
+            // 
+                // case 11:
+                // cout << "nov" << endl;break;
+            // 
+                // case 12:
+                // cout << "dec" << endl;break;
+            // 
+               // default:
+               // cout << "invalid number"<< endl
+            // 
+            // 
+            // }
+        // 
+        // }
---
+++ Day_2.cpp
@@ -0,0 +1,109 @@
+// Manupulation in array
+
+    // #include<bits/stdc++.h>
+    // using namespace std;
+    // int main(){
+      // int arr[5] = {1,2,5,8,9};
+      // cout << arr[0] << endl;
+      // cout << arr[4] << endl;
+      // arr[0] = arr[0]+10;
+      // cout << arr[0] << endl;
+    // 
+      // return 0;
+    // };
+
+// key points of array
+    // -> datatype will be same.
+    // -> element or the value can be change.
+    // -> memory adjucent to each other.  
+
+// example:-
+    // #include<bits/stdc++.h>
+    // using namespace std;
+    // int main(){
+      // string s = "sameer kampa";
+      // int len = s.size();
+      // cout << len << endl;
+      // cout << s[len - 6] << endl;
+      // return 0;
+    // }
+
+// for loop in c++
+    // #include<bits/stdc++.h>
+    // using namespace std;
+    // int main(){
+      // for(int i=0;i <= 10;i++){
+        // cout << i << endl;
+      // }
+      // return 0;
+    // }
+
+// using while loop
+    // #include<bits/stdc++.h>
+    // using namespace std;
+    // int main(){
+      // int i = 2;
+      // while(i <= 10){
+        // cout << i << endl;
+        // i = i + 1;
+      // }
+    // }
+
+//using do-while loop
+    // #include<bits/stdc++.h>
+    // using namespace std;
+    // int main(){
+      // int i= 1;
+      // do {
+        // cout << i <<endl;
+      // 
+      // }while(i >= 5);
+      // return 0;
+    // }
+
+// using Function 
+    // #include<bits/stdc++.h>
+    // using namespace std;
+    // int addition(int a , int b){
+      // long long c = a + b;
+      // return c;
+    // 
+    // }
+    // int main(){
+      // cout << addition(45,75) << endl;
+      // return 0;
+    // }
+
+
+// using function
+    // #include<bits/stdc++.h>
+    // using namespace std;
+    // 
+    // int increment(int &a){
+      // a = a+1;
+      // cout << "inside function "<< a << endl;
+      // return a;
+    // }
+    // int main(){
+      // int a = 3;
+      // increment(a);
+      // cout << "inside main " << a <<endl;
+      // return 0;
+    // } 
+
+// using function
+    // #include<bits/stdc++.h>
+    // using namespace std;
+    // 
+    // 
+    // int increment(int a[]){
+      // a[2] = a[2] + 10;
+      // cout << "inside function "<< a[2] << endl;
+      // return a[2];
+    // }
+    // int main(){
+      // int a[5] = {1,2,3,4,5};
+      // increment(a);
+      // cout << "inside main " << a[2] <<endl;
+      // return 0;
+    // } 
