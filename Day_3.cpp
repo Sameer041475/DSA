95825ad921bad39854b84ce5ea5c1015f5475ba1
Sameer Kampa <kampasameer95@gmail.com>
2026-05-03 00:09:44 +0530

    added Day_3.cpp

---
+++ Day_3.cpp
@@ -0,0 +1,141 @@
+// 1D array
+
+        // #include<bits/stdc++.h>
+        // using namespace std;
+        
+        // int main() {
+        //     int arr[5] = {1,2,3,4,5};
+        //     cout << arr[0]<<endl;
+        //     cout << arr[3] << endl;
+            
+        //     arr[0] = arr[0] + 10;
+        //     cout << arr[0] << endl;
+        //   return 0;
+        // }
+
+
+
+//  2D array
+
+        // #include<bits/stdc++.h>
+        // using namespace std;
+        
+        // int main() {
+        //     int arr[5][3] = {
+        //         {1,2,3},
+        //         {4,5,0},
+        //         {0,0,0},
+        //         {0,0,0},
+        //         {0,0,0}
+        //     };
+        
+        //     cout << arr[0][0] << endl;  // 1
+        //     cout << arr[1][0] << endl;  // 4
+        
+        //     arr[0][0] = arr[0][0] + 10;
+        
+        //     cout << arr[0][0] << endl;  // 11
+        
+        //     return 0;
+        // }
+
+
+// string indexing
+
+        // #include <bits/stdc++.h>
+        // using namespace std;
+        // int main() {
+        //     string s = "sameer";
+        //     int len = s.size();
+        //     cout << len << endl;
+        //     s[len - 1] = 'i';
+        //     cout << s << endl;
+        //     return 0 ;
+        // }
+        
+        
+//  FOR LOOP
+
+        // #include <bits/stdc++.h>
+        // using namespace std;
+        // int main() {
+        //     for(int i = 1; i <= 10; i++){
+        //         cout << i << endl;
+        //     }
+        //     return 0 ;
+        // }
+
+// WHILE LOOP
+
+        // #include <bits/stdc++.h>
+        // using namespace std;
+        // int main() {
+        //     int i = 1;
+        //     while(i < 10){
+                
+        //         cout << i << endl;
+        //         i += 1;
+        //     }
+        //     return 0 ;
+        // }
+
+//  DO while
+
+        // #include <bits/stdc++.h>
+        // using namespace std;
+        // int main() {
+        //     int i = 1;
+        //     do{
+        //         cout << i << endl;
+        //     }while(i >= 5);
+            
+        //     return 0 ;
+        // }     
+                
+// FUNCTION IN C++
+
+        // #include<bits/stdc++.h>
+        // using namespace std;
+        // int addition(int a, int b){
+        //     int c = a + b;
+        //     return c;
+        // }
+        // int main(){
+        //     cout << addition(2,3) << endl;
+        // }
+
+// INSIDE FUNNCTION
+
+        // #include<bits/stdc++.h>
+        // using namespace std;
+        
+        // int increment(int a){
+        //     a = a + 2;
+        //     cout << a << endl;
+        //     return a;
+        // }
+        // int main(){
+        //     int a = 3;
+        //     increment(a);
+        //     cout << a << endl;
+        //     return 0;
+        // }
+        
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
+        
+        
