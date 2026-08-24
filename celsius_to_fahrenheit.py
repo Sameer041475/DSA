852bbd00b58e1b57c79e27cea76fc7d4e0ded3ed
Sameer Kampa <kampasameer95@gmail.com>
2026-08-24 18:24:23 +0530

    DSA

---
+++ celsius_to_fahrenheit.py
@@ -0,0 +1,3 @@
+A = int(input())
+F = A * 9/5 + 32
+print("Cold") if F < 50 else print("Warm")
---
+++ sign_indicator.py
@@ -0,0 +1,2 @@
+N = int(input())
+print("Positive") if N > 0 else print("Negative") if N < 0 else print("Zero")
